e2770f6a46e8cea8b1a2d13eaa50e8c43a3fd6c5
aigumentsev <igumenwork@mail.ru>
2020-01-26 11:44:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2___test_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
