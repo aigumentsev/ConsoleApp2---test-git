5c9b68ee977105162610c2ed0231c2275a5a1679
aigumentsev <igumenwork@mail.ru>
2020-01-26 22:32:59 +0300

    push to local branch1 added

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ConsoleApp2___test_git
         {
             Console.Write("test commit1");
             Console.Write("push to release 0.1");
-//            Console.Write("push to local branch1 added");
+            Console.Write("push to local branch1 added");
         }
     }
 }
